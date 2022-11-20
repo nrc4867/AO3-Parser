0899236e7f8ada936c706daa48ca4d49bc101f97
nrc4867 <nrc4867@rit.edu>
2022-11-19 20:47:45 -0500

    Null Check Changes
    Signed-off-by: nrc4867 <nrc4867@rit.edu>

--- src/main/java/dev/chieppa/wrapper/parser/PageExtractor.kt
+++ src/main/java/dev/chieppa/wrapper/parser/PageExtractor.kt
@@ -12,8 +12,8 @@ fun extractPage(navigation: Element?): Navigation {
 
     navigation?.let {
         page = it.getElementsByClass("current").getOrNull(0)?.text()?.toInt() ?: Int.MAX_VALUE
-        val pageButtons = it.getFirstByClass("next")
-        pages = pageButtons.previousElementSibling()?.text()?.toInt() ?: 0
+        val pageButtons = it.getElementsByClass("next").first()
+        pages = pageButtons?.previousElementSibling()?.text()?.toInt() ?: 0
     }
 
     return Navigation(page, pages)
